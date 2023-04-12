ae87c393346ca5d19836deded76aeb4bc7557ff9
GnailiyLin <gnailiylinwiz@gmail.com>
2023-04-12 15:54:11 +0800

    feat: add evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"panda/evaluator"
 	"panda/lexer"
+	"panda/object"
 	"panda/parser"
 )
 
@@ -21,6 +23,7 @@ const Panda = `
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	fmt.Fprintf(out, Panda)
 
@@ -42,8 +45,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
